e62a8903cd9042ab5f7126b20495240eb0d98366
Chris Li <lihongyu0807@icloud.com>
2020-11-08 04:58:53 +0800

    docker containerize

---
+++ run_docker.cmd
@@ -0,0 +1,2 @@
+docker pull lihongyu0807/ucpweb:latest
+docker run -p 5100:5100 ucpweb --name=ucpweb
---
+++ run_docker.sh
@@ -0,0 +1,2 @@
+docker pull lihongyu0807/ucpweb:latest
+docker run -p 5100:5100 --name=ucpweb lihongyu0807/ucpweb
